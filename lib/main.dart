c7411e3fab222a21557cf96f00daa405c3ad8755
Unyc1124 <aditidubey792@gmail.com>
2024-12-17 12:32:33 +0530

    final_auth

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,16 @@ import 'package:book_app/pages/shop_page.dart';
 import 'package:book_app/themes/light_mode.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:book_app/pages/login_page.dart';
+import 'package:book_app/pages/signup_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(ChangeNotifierProvider(
     create: (context) => Shop(),
     child: const MyApp(),
@@ -21,10 +29,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Readers Heaven",
-      home: ShopBag(),
+      home:const IntroPage(),
       theme: lightmode,
       routes: {
         'intro_page': (context) => IntroPage(),
+        '/login_page': (context) => const LoginPage(), // Existing Login Page
+        '/signup_page': (context) => const SignupPage(), // Exist
         '/shop_page': (context) => ShopBag(),
         '/cart_page': (context) => CartPage(),
       },
--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -1,6 +1,5 @@
 import 'package:book_app/components/my_drawer.dart';
 import 'package:book_app/components/my_product_tile.dart';
-import 'package:book_app/models/product.dart';
 import 'package:book_app/models/shop.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -17,17 +16,17 @@ class ShopBag extends StatelessWidget {
           backgroundColor: Colors.transparent,
           elevation: 0,
           foregroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text("Shop Bag"),
+          title: const Text("Shop Bag"),
           actions: [
             // cart button
             IconButton(
                 padding: const EdgeInsets.only(right: 25.0),
                 onPressed: () => Navigator.pushNamed(context, '/cart_page'),
-                icon: Icon(Icons.shopping_cart_checkout))
+                icon: const Icon(Icons.shopping_cart_checkout))
           ],
         ),
         drawer: const MyDrawer(),
-        backgroundColor: Theme.of(context).colorScheme.background,
+        backgroundColor: Theme.of(context).colorScheme.surface,
         body: ListView(
           children: [
             //shop subtitle
